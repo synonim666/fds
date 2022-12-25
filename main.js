225389601b641623a92e8518525bd3e3118ff3c6
nikita <bf3122@gmail.com>
2022-12-25 08:00:51 -0800

    release

---
+++ main.js
@@ -0,0 +1,47 @@
+// console.log(1);
+// console.log('nikita');/
+
+// console.log(true);
+// console.log(false);
+
+// var imya = 'nikita';
+
+// var myAgeIs = 13
+
+// console.log(imya);
+// console.log(myAgeIs);
+
+// console.log('Я '+imya + ' мне '+ myAgeIs);
+
+/* var str = prompt('Введите имя')
+console.log(str);
+
+alert(str); */
+
+/* console.log('10 + 3 = ' + (10 + 3)); */
+
+var str = prompt('input your name')
+console.log('меня зовут ' + str);
+
+var strt = prompt('Введите свой возвраст')
+console.log('мой возраст ' +  strt);
+
+var strs = prompt('пожалуйста решите пример на сложение ' + ' 12+3 ')
+console.log( ' ваш ответ:' +' 12+3='+ strs +' правильный ответ: '+' 12+3='+(12+3));
+
+var strq = prompt('пожалуйста решите пример на вычитание ' + ' 16-4 ')
+console.log( ' ваш ответ:' +' 16-4='+ strq +' правильный ответ: '+' 16-4='+(16-4));
+
+var strw = prompt('пожалуйста решите пример на умножение ' + ' 12*17 ')
+console.log( ' ваш ответ:' +' 12*17='+ strw +' правильный ответ: '+' 12*17='+(12*17));
+
+var stre = prompt('пожалуйста решите пример на деление ' + ' 12/4 ')
+console.log( ' ваш ответ:' +' 12/4='+ stre +' правильный ответ: '+' 12/4='+(12/4));
+
+var strr = prompt('пожалуйста решите пример на деление c остатком ' + ' 12/7 ')
+console.log( ' ваш ответ:' +'12/7='+ strr +' правильный ответ: '+' 12/7='+(12/7));
+
+alert('пожалуйста откройте консоль,чтобы сравнить свои ответы c правильными(f12)');
+
+
+
